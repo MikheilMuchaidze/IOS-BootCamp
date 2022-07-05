f344aef6ce49404f48e138751b14f4f12dd7759a
Mikheil Muchaidze <102153227+MikheilMuchaidze@users.noreply.github.com>
2022-07-05 10:24:19 +0400

    Add files via upload

---
+++ Lecture 13/Muchaidze_Lecture_13/Muchaidze_Lecture_13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Muchaidze_Lecture_13
+//
+//  Created by Misha Muchaidze on 20220705...
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
