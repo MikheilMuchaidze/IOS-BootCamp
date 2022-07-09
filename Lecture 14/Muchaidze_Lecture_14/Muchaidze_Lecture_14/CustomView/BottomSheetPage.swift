2aa3497ab91e6eb7dbdf0c770f423cf6872bcbf4
Mikheil_Muchaidze <mikheil.muchaidze@gmail.com>
2022-07-10 01:00:12 +0400

    added bottom shee page custom view and swift file to manage logout from details page

---
+++ Lecture 14/Muchaidze_Lecture_14/Muchaidze_Lecture_14/CustomView/BottomSheetPage.swift
@@ -0,0 +1,20 @@
+//
+//  BottomSheetPage.swift
+//  Muchaidze_Lecture_14
+//
+//  Created by Misha Muchaidze on 20220710...
+//
+
+import UIKit
+
+class BottomSheetPage: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
