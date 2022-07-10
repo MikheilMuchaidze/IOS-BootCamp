e774ee2f90444aec2639a94d1a38041bb2edd923
Mikheil_Muchaidze <mikheil.muchaidze@gmail.com>
2022-07-10 14:17:30 +0400

    added pop up content on details page with funcionality- no: closes popup, yes: goes to login page. There are bugs but I think It works pretty well for what it is. I'm happy ^_^

--- Lecture 14/Muchaidze_Lecture_14/Muchaidze_Lecture_14/ViewControllers/DetailsPage.swift
+++ Lecture 14/Muchaidze_Lecture_14/Muchaidze_Lecture_14/ViewControllers/DetailsPage.swift
@@ -12,6 +12,9 @@ class DetailsPage: UIViewController {
     @IBOutlet weak var userNameDetails: UILabel! 
     @IBOutlet weak var userEmailDetails: UILabel!
     
+    @IBOutlet weak var blurView: UIView!
+    @IBOutlet var popupView: UIView!
+    
     var userNameFromRegistrationDetails = ""
     var userEmailFromRegistrationDetails = ""
         
@@ -22,10 +25,60 @@ class DetailsPage: UIViewController {
         userNameDetails.text = userNameFromRegistrationDetails
         print(userEmailFromRegistrationDetails)
         userEmailDetails.text = userEmailFromRegistrationDetails
-    }
         
-    @IBAction func signOutButton(_ sender: Any) {
-        let popup = BottomSheetPage()
-        view.addSubview(popup)
+        //sets the size of the blur view to be equal to size of overall view
+        blurView.bounds = self.view.bounds
+        
+        //set width = 90% of the screen, height = 40% of the screen
+        popupView.bounds = CGRect(x: 0, y: 0, width: 240, height: 130)
     }
+    
+    //Pop-up actions are made with help of this video: https://www.youtube.com/watch?v=gLTDY8Qj6EM
+    
+    //animate in a specified view
+    func animateIn(desiredView: UIView) {
+        let backgroundView = self.view!
+        
+        //attach our desired view to the screen (self.view/backgroundView)
+        backgroundView.addSubview(desiredView)
+        
+        //sets the view's scaling to be 120%
+        desiredView.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+        desiredView.alpha = 0
+        desiredView.center = backgroundView.center
+        
+        //animate the affect
+        UIView.animate(withDuration: 0.3, animations: {
+            desiredView.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+            desiredView.alpha = 1
+        })
+    }
+    
+    //animate out a specified view
+    func animateOut(desiredView: UIView) {
+        UIView.animate(withDuration: 0.3, animations: {
+            desiredView.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+            desiredView.alpha = 0
+        }, completion: { _ in
+            desiredView.removeFromSuperview()
+        })
+    }
+    
+    //ratomgac bluris epeqti ar edeba ?????
+    @IBAction func signOutAction(_ sender: Any) {
+        animateIn(desiredView: blurView)
+        animateIn(desiredView: popupView)
+    }
+    
+    @IBAction func singOutNo(_ sender: Any) {
+        animateOut(desiredView: popupView)
+        animateOut(desiredView: blurView)
+    }
+    
+    
+    @IBAction func singOutYes(_ sender: Any) {
+        let goToLoginPage = storyboard?.instantiateViewController(withIdentifier: "LoginPage") as? LoginPage
+        self.navigationController?.pushViewController(goToLoginPage!, animated: true)
+    }
+    
 }
