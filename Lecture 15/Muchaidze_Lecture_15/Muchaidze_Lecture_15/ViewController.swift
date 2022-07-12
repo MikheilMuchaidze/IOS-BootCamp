20b223f52ba8213e77d2958f5ca37a08980a32e1
Mikheil_Muchaidze <mikheil.muchaidze@gmail.com>
2022-07-12 18:18:48 +0400

    starting 15th homework

---
+++ Lecture 15/Muchaidze_Lecture_15/Muchaidze_Lecture_15/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Muchaidze_Lecture_15
+//
+//  Created by Misha Muchaidze on 20220712...
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
