4719c06537f60baceffaa81df4ed75fbead2e94b
Mikheil_Muchaidze <mikheil.muchaidze@gmail.com>
2022-07-16 01:11:13 +0400

    test commit for lecture 17

---
+++ Lecture 17/Lecture_17_test/Lecture_17_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lecture_17_test
+//
+//  Created by Misha Muchaidze on 20220715...
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
