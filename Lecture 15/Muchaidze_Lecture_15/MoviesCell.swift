079a5293141055609505b3572784222f835c9662
Mikheil_Muchaidze <mikheil.muchaidze@gmail.com>
2022-07-22 20:30:54 +0400

    added categories to movies list

--- Lecture 15/Muchaidze_Lecture_15/MoviesCell.swift
+++ Lecture 15/Muchaidze_Lecture_15/MoviesCell.swift
@@ -11,15 +11,18 @@ class MoviesCell: UITableViewCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
+        // Configure the view for the selected states
     }
     
     @IBOutlet weak var titleLbl: UILabel!
     @IBOutlet weak var imdbLbl: UILabel!
     
     @IBAction func seenUnseenBtn(_ sender: Any) {
+        
     }
     
     
     
 }
+
+
--- Lecture 15/Muchaidze_Lecture_15/Muchaidze_Lecture_15/PagesControllers/MovieListController.swift
+++ Lecture 15/Muchaidze_Lecture_15/Muchaidze_Lecture_15/PagesControllers/MovieListController.swift
@@ -1,32 +1,60 @@
 
 import UIKit
 
-class MovieListController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-        
+class MovieListController: UIViewController {
+            
     @IBOutlet weak var mainTableView: UITableView!
         
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mainTableView.delegate = self
+        mainTableView.dataSource = self
+    }
+    
+}
+
+extension MovieListController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return moviesList.count - moviesSeen.count
+        case 1:
+            return moviesSeen.count
+        default:
+            return 0
+        }
+    }
+    
     func numberOfSections(in tableView: UITableView) -> Int {
         return 2
     }
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        moviesList.count
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let labelNotSeen = UILabel()
+        labelNotSeen.text = "Not seen"
+        labelNotSeen.backgroundColor = UIColor.green
+        
+        let labelSeen = UILabel()
+        labelSeen.text = "Seen"
+        labelSeen.backgroundColor = UIColor.gray
+        
+        if section == 0 {
+            return labelNotSeen
+        } else {
+            return labelSeen
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MoviesCell", for: indexPath) as! MoviesCell
-        let thisMovies = moviesList[indexPath.row]
-        cell.titleLbl.text = thisMovies.title
+        
+        let thisMovies = indexPath.section == 0 ? moviesSeen[indexPath.row] : moviesNotSeen[indexPath.row]
+        
+        cell.titleLbl.text = "\(thisMovies.title)"
         cell.imdbLbl.text = "\(thisMovies.imdb)"
         return cell
-    }
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Seen/Unseen test"
-    }
-        
-    override func viewDidLoad() {
-        super.viewDidLoad()
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -37,7 +65,7 @@ class MovieListController: UIViewController, UITableViewDelegate, UITableViewDat
         if (segue.identifier == "detailSegue") {
             let indexPath = self.mainTableView.indexPathForSelectedRow!
             let tableViewDetail = segue.destination as? DetailsPageController
-            
+
             let selectedMovie = moviesList[indexPath.row]
             tableViewDetail!.moviesList = selectedMovie
             self.mainTableView.deselectRow(at: indexPath, animated: true)
