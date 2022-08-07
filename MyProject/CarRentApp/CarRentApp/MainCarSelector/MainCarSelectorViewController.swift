4940f6864b879558229fc65bd7a51800d751597f
Mikheil_Muchaidze <mikheil.muchaidze@gmail.com>
2022-08-08 01:52:26 +0400

    login register page more or less ready

---
+++ MyProject/CarRentApp/CarRentApp/MainCarSelector/MainCarSelectorViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainCarSelectorViewController.swift
+//  CarRentApp
+//
+//  Created by Misha Muchaidze on 20220803...
+//
+
+import UIKit
+
+class MainCarSelectorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
