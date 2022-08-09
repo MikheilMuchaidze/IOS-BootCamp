4e4e9fc96ee517d374b7335a552b3d1bbef9a648
Mikheil_Muchaidze <mikheil.muchaidze@gmail.com>
2022-08-09 22:25:43 +0400

    added icons for logout and profile to car selector view

--- MyProject/CarRentApp/CarRentApp/MainCarSelector/MainCarSelectorViewController.swift
+++ MyProject/CarRentApp/CarRentApp/MainCarSelector/MainCarSelectorViewController.swift
@@ -2,24 +2,44 @@ import UIKit
 
 class MainCarSelectorViewController: UIViewController {
     
-    @IBOutlet weak var backBtnImage: UIImageView!
-
+    @IBOutlet weak var logoutBtnImage: UIImageView!
+    @IBOutlet weak var profileBtnImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        addTapToBackPic()
+        //adding userinteractions to logout and profile images
+        logoutBtnImage.isUserInteractionEnabled = true
+        addTapTologoutImage()
+        profileBtnImage.isUserInteractionEnabled = true
+        addTapToAccountImage()
+
 
     }
     
 }
 
 extension MainCarSelectorViewController {
-    func addTapToBackPic() {
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapGoBack))
-        backBtnImage.addGestureRecognizer(tapGesture)
+    //add tap function to logout image: goes back to sign in page
+    func addTapTologoutImage() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapToLogout))
+        logoutBtnImage.addGestureRecognizer(tapGesture)
     }
     
-    @objc func tapGoBack() {
+    @objc func tapToLogout() {
         self.navigationController?.popViewController(animated: true)
     }
+    
+    //add tap function to account details image: goes to details page
+    func addTapToAccountImage() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapToGoAccDetails))
+        profileBtnImage.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func tapToGoAccDetails() {
+        let goToDetailsPage = storyboard?.instantiateViewController(withIdentifier: "ProfileDetails") as? ProfileDetails
+        self.navigationController?.present(goToDetailsPage!, animated: true)
+    }
+    
 }
---
+++ MyProject/CarRentApp/CarRentApp/ProfileDetails/ProfileDetails.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class ProfileDetails: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
