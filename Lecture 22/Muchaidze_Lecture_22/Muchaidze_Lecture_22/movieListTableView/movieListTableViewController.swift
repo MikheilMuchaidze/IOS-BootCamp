c9b27fe97e4c47a758bb4bced0f9cd3ac8297223
Mikheil_Muchaidze <mikheil.muchaidze@gmail.com>
2022-08-13 16:51:57 +0400

    first try to do 22th homework

---
+++ Lecture 22/Muchaidze_Lecture_22/Muchaidze_Lecture_22/movieListTableView/movieListTableViewController.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+class movieListTableViewController: UIViewController {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        tableView.delegate = self
+//        tableView.dataSource = self
+
+    }
+
+}
+
