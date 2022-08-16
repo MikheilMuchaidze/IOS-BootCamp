7c52cbb1bc9280f3297132ba52d64e03ce5e5806
Mikheil_Muchaidze <mikheil.muchaidze@gmail.com>
2022-08-16 18:58:50 +0400

    first try to write code for 23th homework...

---
+++ Lecture 23/Muchaidze_Lecture_23/Muchaidze_Lecture_23/NetworkCall/NetworkCall.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class NetworkCall {
+    
+    func getDataFromAPI<T: Codable>(fromUrlString: String, complitionHandler: @escaping (T) -> (Void)) {
+        
+        
+        
+        
+        
+    }
+    
+}
---
+++ Lecture 23/Muchaidze_Lecture_23/Muchaidze_Lecture_23/ViewController.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let API_KEY = "c78b72820646a4860d6b67948bccc37f"
+    let topRatedMovies = "https://developers.themoviedb.org/3/tv/get-top-rated-tv"
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
+
