d94e98e96e65b6590d4cd288ff5df0bb79bbb307
Mikheil_Muchaidze <mikheil.muchaidze@gmail.com>
2022-08-24 10:41:57 +0400

    added funcionality to add category and note itself from textfields

--- Lecture 25/Muchaidze_Lecture_25/Muchaidze_Lecture_25/ViewController.swift
+++ Lecture 25/Muchaidze_Lecture_25/Muchaidze_Lecture_25/ViewController.swift
@@ -1,13 +1,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var noteInputTxt: UITextField!
+    @IBOutlet weak var noteCategoryTxt: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
 
     }
 
+    
+    @IBAction func addNoteBtn(_ sender: Any) {
+        
+        let fileManager = FileManager.default
+        guard let url = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else { return }
+        
+        guard let note = noteInputTxt.text else { return }
+        guard let noteCategory = noteCategoryTxt.text else { return }
+        
+        //ახალი ფაილი 📁
+        let newFolderUrl = url.appendingPathComponent(noteCategory)
+        
+        try? fileManager.createDirectory(at: newFolderUrl,
+                                    withIntermediateDirectories: true,
+                                    attributes: [:])
+        
+        let fileUrl = newFolderUrl.appendingPathComponent(note)
+        let noteData = note.data(using: .utf8)!
+        fileManager.createFile(atPath: fileUrl.path,
+                               contents: noteData,
+                               attributes: [:])
+        
+    }
+    
 
 }
 
